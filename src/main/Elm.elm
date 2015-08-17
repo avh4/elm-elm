ed38707fd0522da491199d6892324b264aea314d
Aaron VonderHaar <gruen0aermel@gmail.com>
2015-08-16 19:15:01 -0700

    Extract Elm.Expression

--- src/main/Elm.elm
+++ src/main/Elm.elm
@@ -1,10 +1,11 @@
 module Elm where
 
+import Elm.Expression as Expression exposing (Expression)
+
 import String
 
 type ElmModule = ElmModule String (List Definition)
 type Definition = Definition String Expression
-type Expression = LiteralInt Int
 
 parse : String -> ElmModule
 parse _ = module' {name="", imports=[], declarations=[]}
@@ -12,15 +13,9 @@ parse _ = module' {name="", imports=[], declarations=[]}
 
 -- TO STRING
 
-expressionToString : Expression -> String
-expressionToString expression =
-    case expression of
-        LiteralInt n -> Basics.toString n
-
-
 declarationToString : Definition -> String
 declarationToString (Definition name value) =
-    name ++ " = " ++ (expressionToString value)
+    name ++ " = " ++ (Expression.toString value)
 
 
 toString : ElmModule -> String
@@ -44,4 +39,4 @@ definition {name,value} =
 
 
 literalNumber : Int -> Expression
-literalNumber = LiteralInt
+literalNumber = Expression.literalNumber
